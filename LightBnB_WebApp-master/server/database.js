9494c640f4cd0b59afa37092686441a229e0bed4
Junsung Park <zeipar@gmail.com>
2020-04-02 23:12:36 +0000

    updated addProperty() to add a row to the DB

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -128,7 +128,7 @@ const getAllProperties = (options, limit = 10) => {
   Object.keys(options).length
 
   if (options.owner_id) {
-    
+
     queryParams.push(`${options.owner_id}`);
     queryString += `${conditionalStatement()} owner_id = $${queryParams.length} `;
   }
@@ -173,9 +173,26 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = property => {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(
+    `
+    INSERT INTO properties 
+      (title, description, owner_id, cover_photo_url, 
+        thumbnail_photo_url, cost_per_night, parking_spaces, 
+        number_of_bathrooms, number_of_bedrooms, 
+        province, city, country, street, post_code) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `,
+    [
+      property.title, property.description, property.owner_id,
+      property.cover_photo_url, property.thumbnail_photo_url,
+      property.cost_per_night, property.parking_spaces,
+      property.number_of_bathrooms, property.number_of_bedrooms,
+      property.province, property.city, property.country,
+      property.street, property.post_code
+    ]
+  )
+    .then(res => res.rows)
+    .catch(err => console.error(err));
 };
 exports.addProperty = addProperty;
